32e2aa79fbf47dc240a2eef32a7f9de799007d11
asmi046 <asmi046@gmail.com>
2024-02-25 19:30:46 +0300

    Пути

---
+++ resources/views/components/widget-file.blade.php
@@ -0,0 +1,6 @@
+<div class="doc-link">
+    <span class="doc-link__caption">
+        {{ $title }}
+    </span>
+    <a href="{{ $lnk }}" class="doc-link__download">Скачать</a>
+</div>
--- routes/cabinet.php
+++ routes/cabinet.php
@@ -13,4 +13,13 @@
         Route::get('/projects', [ProjectController::class, "index"])->name('projects');
         Route::get('/projects/create', [ProjectController::class, "index"])->name('project_create');
         Route::get('/projects/edit/{id}', [ProjectController::class, "index"])->name('project_edit');
+
+        Route::get('/support', [GossSupportController::class, "index"])->name('support');
+        Route::get('/support/create', [GossSupportController::class, "create"])->name('support_create');
+        Route::get('/support/edit/{id}', [GossSupportController::class, "edit"])->name('support_edit');
+
+        Route::get('/technical_connect', [TechnicalConnectController::class, "index"])->name('technical_connect');
+        Route::get('/technical_connect/select', [TechnicalConnectController::class, "select"])->name('technical_connect_select');
+        Route::get('/technical_connect/create', [TechnicalConnectController::class, "create"])->name('technical_connect_create');
+        Route::get('/technical_connect/edit/{id}', [TechnicalConnectController::class, "edit"])->name('technical_connect_edit');
     });
