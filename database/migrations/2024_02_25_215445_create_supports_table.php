8f5ed629a380c3c2e04e3b20c1bfb2d0d7cb4406
asmi046 <asmi046@gmail.com>
2024-02-27 17:35:39 +0300

    Инвестиционный проект таблица

--- database/migrations/2024_02_25_215445_create_supports_table.php
+++ database/migrations/2024_02_25_215445_create_supports_table.php
@@ -14,6 +14,20 @@ return new class extends Migration
         Schema::create('supports', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string("organization")->comment('Наименование организации');
+            $table->string("name")->comment('Название инвестиционного проекта');
+            $table->integer("invest_volume")->comment('Объем инвестиций в основной капитал по проекту (с учетом налога на добавленную стоимость), млн. рублей');
+            $table->string("detail",1000)->comment('Инвестиционный проект предусматривает');
+            $table->string("many_src")->comment('Источник средств реализации проекта');
+            $table->integer('time_relis')->comment("Срок реализации инвестиционного проекта");
+            $table->integer('npv')->comment("Чистый приведенный доход проекта (NPV)");
+            $table->integer('irr')->comment("Внутренняя норма доходности проекта (IRR)");
+            $table->integer('time_prib')->comment("Простой срок окупаемости инвестиционного проекта");
+            $table->integer('worck_place_coun')->comment("Количество создаваемых рабочих мест");
+            $table->integer('zp')->comment("Средняя заработная плата по проекту");
+            $table->integer('okved')->comment("Средняя заработная плата по проекту");
+            $table->text('description')->nullable()->comment("Комментарии к проекту");
+
         });
     }
 
