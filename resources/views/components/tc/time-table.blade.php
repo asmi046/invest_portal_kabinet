625be659f56e31f7643092a6bc7ec71667cfc3d5
asmi046 <asmi046@gmail.com>
2024-04-10 09:25:41 +0300

    + value

--- resources/views/components/tc/time-table.blade.php
+++ resources/views/components/tc/time-table.blade.php
@@ -11,25 +11,28 @@
     </thead>
     <tbody>
         <tr>
-            <td><input type="text" name="et_1"></td>
-            <td><input type="text" name="pproject_1"></td>
-            <td><input type="text" name="pexpl_1"></td>
-            <td><input type="text" name="maxp_1"></td>
-            <td><input type="text" name="cat_1"></td>
+            <td><input type="text" name="et_1" value="et_1"></td>
+            <td><input type="text" name="pproject_1" value="pproject_1"></td>
+            <td><input type="text" name="pexpl_1" value="pexpl_1"></td>
+            <td><input type="text" name="maxp_1" value="maxp_1"></td>
+            <td><input type="text" name="cat_1" value="cat_1"></td>
         </tr>
+
         <tr>
-            <td><input type="text" name="et_2"></td>
-            <td><input type="text" name="pproject_2"></td>
-            <td><input type="text" name="pexpl_2"></td>
-            <td><input type="text" name="maxp_2"></td>
-            <td><input type="text" name="cat_2"></td>
+            <td><input type="text" name="et_2" value="et_2"></td>
+            <td><input type="text" name="pproject_2" value="pproject_2"></td>
+            <td><input type="text" name="pexpl_2" value="pexpl_2"></td>
+            <td><input type="text" name="maxp_2" value="maxp_2"></td>
+            <td><input type="text" name="cat_2" value="cat_2"></td>
         </tr>
+
         <tr>
-            <td><input type="text" name="et_3"></td>
-            <td><input type="text" name="pproject_3"></td>
-            <td><input type="text" name="pexpl_3"></td>
-            <td><input type="text" name="maxp_3"></td>
-            <td><input type="text" name="cat_3"></td>
+            <td><input type="text" name="et_3" value="et_3"></td>
+            <td><input type="text" name="pproject_3" value="pproject_3"></td>
+            <td><input type="text" name="pexpl_3" value="pexpl_3"></td>
+            <td><input type="text" name="maxp_3" value="maxp_3"></td>
+            <td><input type="text" name="cat_3" value="cat_3"></td>
         </tr>
+
     </tbody>
 </table>
